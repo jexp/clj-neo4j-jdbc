6824f185eb882867f69075f9f37be1540caee8bb
Michael Hunger <github@jexp.de>
2012-06-22 02:52:55 +0200

    update

--- src/neo4j_jdbc/core.clj
+++ src/neo4j_jdbc/core.clj
@@ -1,12 +1,13 @@
 (ns neo4j-jdbc.core
-;	(:use ring.adapter.jetty)
+   (:use ring.adapter.jetty)
 )
 
 (defn hello [request]
     {:status 200 :headers {"Content-Type" "text/html"} :body "Hello World"}
 )
+
 (defn -main []
-	(let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
-	  (run-jetty hello {:port port})
-	)
+    (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+      (run-jetty hello {:port port})
+    )
 )
