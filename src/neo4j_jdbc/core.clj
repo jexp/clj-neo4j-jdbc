289095505688b4a35308f7effb338dd8da7c603c
Michael Hunger <github@jexp.de>
2012-06-22 08:05:55 +0200

    added clj-http-client

--- src/neo4j_jdbc/core.clj
+++ src/neo4j_jdbc/core.clj
@@ -1,13 +1,20 @@
 (ns neo4j-jdbc.core
-   (:use ring.adapter.jetty)
-)
+    (:use [ring.adapter.jetty] [clojure.data.json])
+    (:require [clj-http.client :as client]))
 
-(defn hello [request]
-    {:status 200 :headers {"Content-Type" "text/html"} :body "Hello World"}
+(defn forward [body]
+	(client/post "http://localhost:7474/db/data/cypher" 
+   		{:accept :json :content-type :json  :body body }))
+)
+(defn forward [request]
+	(let [res (forward (json-str {:query "start n=node(0) return n"}))]
+    	(println res)
+    	{:status 200 :headers {"Content-Type" "text/html"} :body res}
+	)
 )
 
 (defn -main []
     (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
-      (run-jetty hello {:port port})
+      (run-jetty forward {:port port})
     )
 )
